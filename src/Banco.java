386d8582b7c603c0f4cdce96fadf55bc3bd8c3a0
jonas07br <jonasrafa07@gmail.com>
2024-05-13 11:58:18 -0300

    add banco class

---
+++ src/Banco.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+
+    private String nomeBanco;
+    List<Cliente> listaClientes;
+
+    public Banco() {
+        this.listaClientes = new ArrayList<>();
+    }
+
+    public void addCliente(Cliente cliente){
+        this.listaClientes.add(new Cliente());
+    }
+    public void listaClientes(){
+        System.out.println(listaClientes);
+    }
+
+}
