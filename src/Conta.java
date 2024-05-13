fc4c08094e254f1dd600cba09e2c1a122bfb398f
jonas07br <jonasrafa07@gmail.com>
2024-05-13 12:24:43 -0300

    update conta

---
+++ src/Conta.java
@@ -0,0 +1,37 @@
+public class Conta implements IConta{
+    
+    private static final int AGENCIA_PADRAO = 61;
+    private static int numeral;
+    protected int agencia;
+    protected int numConta;
+    protected double saldo;
+    protected Cliente cliente;
+
+    //Constructor
+    public Conta(Cliente cliente) {
+        this.agencia = AGENCIA_PADRAO;
+        this.numConta = numeral++;
+        this.cliente = cliente;
+        this.saldo=0;
+    }
+
+    public void sacar(double valorSaque){
+        saldo-=valorSaque;
+    }
+    public void depositar(double valorDeposito){
+        saldo +=valorDeposito;
+    }
+    public void transferir(double valor){
+        this.sacar(valor);
+    }
+
+    public void imprimirExtrato() {
+        
+    }
+
+    
+
+   
+    
+    
+}
